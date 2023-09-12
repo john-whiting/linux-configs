2b679222e00b391d83f7c46cb65b7a9eefe87461
John Whiting <thejwhiting@gmail.com>
2023-09-12 19:27:53 -0400

    refactor: made indentation consistent in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@
 INSTALL_PATH=${INSTALL_PATH:=~/.jwhiting}
 
 bind_config() {
-	FROM_PATH=$1
-	TO_PATH=$INSTALL_PATH/$2
-	echo "Binding config for $FROM_PATH to $TO_PATH"
+  FROM_PATH=$1
+  TO_PATH=$INSTALL_PATH/$2
+  echo "Binding config for $FROM_PATH to $TO_PATH"
 
-	TO_DIR=$(dirname $TO_PATH)
+  TO_DIR=$(dirname $TO_PATH)
 
-	mkdir $TO_DIR -p
+  mkdir $TO_DIR -p
 
-	rm $FROM_PATH.old &> /dev/null
-	mv $FROM_PATH $FROM_PATH.old &> /dev/null
-	ln -s $TO_PATH $FROM_PATH
+  rm $FROM_PATH.old &> /dev/null
+  mv $FROM_PATH $FROM_PATH.old &> /dev/null
+  ln -s $TO_PATH $FROM_PATH
 }
 
 check_fail() {
@@ -60,70 +60,70 @@ pull_github() {
 
 install_nvim() {
   echo "Installing neovim"
-	# Remove the configurations/state files
-	rm ~/.config/nvim -r &> /dev/null
-	rm ~/.local/share/nvim &> /dev/null
-	rm ~/.local/state/nvim &> /dev/null
+  # Remove the configurations/state files
+  rm ~/.config/nvim -r &> /dev/null
+  rm ~/.local/share/nvim &> /dev/null
+  rm ~/.local/state/nvim &> /dev/null
 
-	# Remove the installation
-	rm ~/.nvim -r &> /dev/null
+  # Remove the installation
+  rm ~/.nvim -r &> /dev/null
 
-	# Install neovim
-	mkdir -p ~/.nvim
+  # Install neovim
+  mkdir -p ~/.nvim
 
-	wget -qO- https://github.com/neovim/neovim/releases/download/v0.9.2/nvim-linux64.tar.gz | tar xvz -C ~/.nvim > /dev/null
+  wget -qO- https://github.com/neovim/neovim/releases/download/v0.9.2/nvim-linux64.tar.gz | tar xvz -C ~/.nvim > /dev/null
 
   echo "Failed to install neovim"
 
-	mv ~/.nvim/nvim-linux64/* ~/.nvim
-	rm ~/.nvim/nvim-linux64 -r
+  mv ~/.nvim/nvim-linux64/* ~/.nvim
+  rm ~/.nvim/nvim-linux64 -r
 
-	# Bind nvim config
+  # Bind nvim config
   mkdir -p ~/.config
-	ln -s $INSTALL_PATH/nvim ~/.config/nvim
+  ln -s $INSTALL_PATH/nvim ~/.config/nvim
   echo "Finished installing neovim"
 }
 
 install_packages() {
-	##
-	# Ensure Repositories are up to date
-	# and install required packages:
-	# Packages include:
-	# - ZSH
-	# - Neovim
-	# - Ripgrep
-	##
-	echo "Installing Packages"
+  ##
+  # Ensure Repositories are up to date
+  # and install required packages:
+  # Packages include:
+  # - ZSH
+  # - Neovim
+  # - Ripgrep
+  ##
+  echo "Installing Packages"
 
-	sudo apt update -y > /dev/null
-	sudo apt install -y zsh ripgrep fzy > /dev/null
+  sudo apt update -y > /dev/null
+  sudo apt install -y zsh ripgrep fzy > /dev/null
 
   # Install ZPlug for ZSH plugins
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-	install_nvim
+  install_nvim
 
-	echo "Finished Installing Packages"
+  echo "Finished Installing Packages"
 }
 
 bind_configs() {
-	##
-	# Bind the necessary configs
-	##
-	echo "Binding Configs"
+  ##
+  # Bind the necessary configs
+  ##
+  echo "Binding Configs"
 
-	# ZSH Config
-	bind_config ~/.zshrc zsh/init.zsh
+  # ZSH Config
+  bind_config ~/.zshrc zsh/init.zsh
 
-	echo "Finished Binding Configs"
+  echo "Finished Binding Configs"
 }
 
 install_plugins() {
-    echo "Installing plugins"
-    # Install NeoVim Plugins
-    ~/.nvim/bin/nvim --headless "+Lazy! sync" +qa > /dev/null
-    check_fail "Failed to install Neovim plugins."
-    echo "Finished installing plugins"
+  echo "Installing plugins"
+  # Install NeoVim Plugins
+  ~/.nvim/bin/nvim --headless "+Lazy! sync" +qa > /dev/null
+  check_fail "Failed to install Neovim plugins."
+  echo "Finished installing plugins"
 }
 
 ##
@@ -131,14 +131,14 @@ install_plugins() {
 ##
 
 main() {
-	echo "Installing JWHITING's Terminal Setup"
+  echo "Installing JWHITING's Terminal Setup"
 
   pull_github
-	install_packages
-	bind_configs
-	install_plugins
+  install_packages
+  bind_configs
+  install_plugins
 
-	echo "Finished Installing JWHITING's Terminal Setup"
+  echo "Finished Installing JWHITING's Terminal Setup"
 }
 
 main
