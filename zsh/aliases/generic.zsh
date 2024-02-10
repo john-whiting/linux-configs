4aac66c2ee97140bac24004254d13c995a292e33
John Whiting <thejwhiting@gmail.com>
2024-02-10 15:41:21 -0500

    feat: added openinwin command

--- zsh/aliases/generic.zsh
+++ zsh/aliases/generic.zsh
@@ -6,3 +6,9 @@ alias zsh-reload="source ~/.zshrc"
 
 #
 alias eth0_ip="ip addr show eth0 | grep \"inet\\b\" | awk '{print \$2}' | cut -d/ -f1"
+
+# Open the PWD inside of Windows Explorer
+openinwin() {
+  DIRECTORY=${$(pwd)//\//\\\\}
+  explorer.exe "\\\\wsl.localhost\Ubuntu$(echo $DIRECTORY)"
+}
